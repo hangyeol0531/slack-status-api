db15e42b9b35d64bb86ff7649710a0d87a64d848
hangyeol0531 <jhan0531@naver.com>
2023-01-08 22:55:13 +0900

    refactor/type, folder 정리

---
+++ src/slack-status/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { slackStatusPng, slackStatusTitle, SlackType, slackTypeMap } from "./types/status";
+
+const slackStatusUrl: string = 'https://status.slack.com/'
+
+const getSlackStatus = async (): Promise<SlackType[]> => {
+    const regex: RegExp = /(?<=<p\sclass="bold">|\/img\/v2\/)[A-Za-z\s\/.]*(?=<\/p>|"\ssrcset="\/img\/v2\/Table)/g;
+    const { data } = await axios.get(slackStatusUrl);
+    const dataRegexList: (slackStatusTitle | slackStatusPng)[] = data.match(regex);
+    const result: SlackType[] = [];
+    for (let i = 5; i < dataRegexList.length; i += 2) {
+        result.push({
+            name: dataRegexList[i] as slackStatusTitle,
+            status: slackTypeMap[dataRegexList[i + 1] as slackStatusPng],
+        })
+    }
+    return result;
+}
+
+(async () => {
+    const result = await getSlackStatus();
+    console.log(result);
+})()
---
+++ src/slack-status/types/status.ts
@@ -0,0 +1,26 @@
+export type slackStatusTitle =
+    'Login/SSO' |
+    'Connections' |
+    'Messaging' |
+    'Link Previews' |
+    'Posts/Files' |
+    'Notifications' |
+    'Calls' |
+    'Search' |
+    'Apps/Integrations/APIs' |
+    'Workspace/Org Administration'
+
+export const slackTypeMap = {
+    'TableCheck.png': 'No Issue',
+    'TableMaintenance.png': 'Maintenance',
+    'TableNotice.png': 'Notice',
+    'TableIncident.png': 'Incident',
+    'TableOutage.png': 'Outage',
+} as const;
+
+export type slackStatusPng = keyof typeof slackTypeMap;
+
+export interface SlackType {
+    name: slackStatusTitle,
+    status: typeof slackTypeMap[slackStatusPng],
+}
