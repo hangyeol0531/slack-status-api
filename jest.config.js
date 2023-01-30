a0aeedd4367a86d5b1deba4683b7a7820d0969af
hangyeol0531 <jhan0531@naver.com>
2023-01-30 23:44:22 +0900

    noIssue TC

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  rootDir: "src",
+  rootDir: "./test",
   testMatch: [
     "**/*.spec.[jt]s?(x)",
     "**/*.test.[jt]s?(x)"
--- test/components/slack-client.spec.ts
+++ test/components/slack-client.spec.ts
@@ -1,7 +1,18 @@
+import axios from 'axios';
+import noIssueHtml from '../data/noIssueHtml.json';
+import noIssue from '../data/noIssue.json';
 import SlackClient from '../../src/slack-status/components/slack-client';
+import { getSlackStatus } from '../../src';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('slack-client', () => {
   it('should have a slack-client', () => {
     expect(typeof SlackClient).toBe('function');
+  });
+  it('should return noIssue status result', async () => {
+    mockedAxios.get.mockResolvedValue(noIssueHtml);
+    expect(await getSlackStatus()).toStrictEqual(noIssue);
   });
 });
