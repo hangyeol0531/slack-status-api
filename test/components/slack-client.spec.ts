8ac91b641aba5a3629371fa0ce4a15fba9893ad3
hangyeol0531 <jhan0531@naver.com>
2023-02-26 22:48:06 +0900

    test/add Posts/Files incident tc

--- test/components/slack-client.spec.ts
+++ test/components/slack-client.spec.ts
@@ -3,10 +3,12 @@ import noIssueHtml from '../data/noIssueHtml.json';
 import incidentHtml from '../data/incidentHtml.json';
 import incidentHtml2 from '../data/incidentHtml2.json';
 import incidentHtml3 from '../data/incidentHtml3.json';
+import incidentHtml4 from '../data/incidentHtml4.json';
 import noIssue from '../data/noIssue.json';
 import incident from '../data/incident.json';
 import incident2 from '../data/incident2.json';
 import incident3 from '../data/incident3.json';
+import incident4 from '../data/incident4.json';
 import SlackClient from '../../src/slack-status/components/slack-client';
 
 jest.mock('axios');
@@ -26,18 +28,23 @@ describe('slack-client', () => {
     expect(await SlackClient.getSlackStatus()).toStrictEqual(noIssue);
   });
 
-  it('should return incident status result ', async () => {
+  it('should return Apps/Integrations/APIs incident status result ', async () => {
     mockedAxios.get.mockResolvedValue(incidentHtml);
     expect(await SlackClient.getSlackStatus()).toStrictEqual(incident);
   });
 
-  it('should return incident status result ', async () => {
+  it('should return Messaging incident status result ', async () => {
     mockedAxios.get.mockResolvedValue(incidentHtml2);
     expect(await SlackClient.getSlackStatus()).toStrictEqual(incident2);
   });
 
-  it('should return incident status result', async () => {
+  it('should return Notifications incident status result', async () => {
     mockedAxios.get.mockResolvedValue(incidentHtml3);
     expect(await SlackClient.getSlackStatus()).toStrictEqual(incident3);
+  });
+
+  it('should return Posts/Files incident status result', async () => {
+    mockedAxios.get.mockResolvedValue(incidentHtml4);
+    expect(await SlackClient.getSlackStatus()).toStrictEqual(incident4);
   });
 });
